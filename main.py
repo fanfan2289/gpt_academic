9ff20922be670c910220e7eed305b3e15e126834
fanfan2289 <110893304+fanfan2289@users.noreply.github.com>
2023-05-10 18:13:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -194,7 +194,7 @@ def main():
         threading.Thread(target=warm_up_modules, name="warm-up", daemon=True).start()
 
     auto_opentab_delay()
-    #demo.queue(concurrency_count=CONCURRENT_COUNT).launch(server_name="0.0.0.0", server_port=PORT, auth=AUTHENTICATION, favicon_path="docs/logo.png")
+    demo.queue(concurrency_count=CONCURRENT_COUNT).launch(server_name="localhost", server_port=PORT, auth=AUTHENTICATION, favicon_path="docs/logo.png")
 
     # 如果需要在二级路径下运行
     # CUSTOM_PATH, = get_conf('CUSTOM_PATH')
